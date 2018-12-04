17a0ed6311a3ed3a9691fa41a4c42dfd79116a6d
Ayush Bahuguna <ayushbahuguna8005@gmail.com>
2018-12-03 23:18:17 -0500

    pre submission

--- Models.py
+++ Models.py
@@ -1,5 +1,6 @@
 import re
 import math
+import collections
 
 
 class LanguageModel:
@@ -17,9 +18,6 @@ class LanguageModel:
         with open(filename, 'w') as f:
             for key in map:
                 f.write(key + ' = ' + str(map[key]) + '\n')
-
-    @staticmethod
-    def write_
 
     @staticmethod
     def clean_sentence(sentence):
@@ -49,31 +47,19 @@ class Unigram(LanguageModel):
                 self.alphabets_map[c] += 1
             else:
                 self.alphabets_map[c] = 1
-
-    def calc_prob_unigram(self, sentence):
-        # sentence = sentence.replace(" ", "").lower()
-        sentence_new = LanguageModel.clean_sentence(sentence)
-        # print(sentence_new)
-        prob = 0;
-        for c in sentence_new:
-            # print('char : ', c)
-            if c in self.alphabets_map:
-                x = (self.alphabets_map[c] + 0.5) / (len(self.text) + 26 * 0.5)
-                prob += math.log(x)
-            else:
-                x = 0.5 / len(self.text)
-                prob += math.log(x)
-        return prob
+        self.generate_probabilities()
 
     def print_dict(self):
         for key in self.alphabets_map:
             print(key, self.alphabets_map[key])
         # print(len(self.alphabets_map))
 
-    def probabilities_alpha(self):
+    def generate_probabilities(self):
+        temp_map = {}
         for key in self.alphabets_map:
-            self.probs_alpha[key] = self.alphabets_map[key] / len(self.text)
-        return self.probs_alpha
+            key2 = '(' + key + ')'
+            temp_map[key2] = (self.alphabets_map[key] + 0.5) / (len(self.text) + 26 * 0.5)
+        self.probs_alpha = collections.OrderedDict(sorted(temp_map.items()))
 
 
 class Bigram(LanguageModel):
@@ -90,6 +76,7 @@ class Bigram(LanguageModel):
             index_j = self.char_hash(self.text[i + 1])
             self.bigram_map[index_i][index_j] += 1
         self.count_char = [sum(i) for i in self.bigram_map]
+        self.generate_conditional_prob()
 
     def char_hash(self, character):
         return self.alphabets.index(character)
@@ -101,18 +88,7 @@ class Bigram(LanguageModel):
         for i in range(self.map_size):
             print(self.bigram_map[i])
 
-    def calc_prob_bigram(self, sentence):
-        sentence_new = LanguageModel.clean_sentence(sentence)
-        # print("Clean sentence", sentence_new)
-        prob = 0
-        for i in range(len(sentence_new) - 1):
-            index_i = self.char_hash(self.text[i])
-            index_j = self.char_hash(self.text[i + 1])
-            x = (self.bigram_map[index_i][index_j] + 0.5) / (self.count_char[index_i] + 26 * 0.5)
-            prob += math.log(x)
-        return prob
-
-    def conditional_prob(self):
+    def generate_conditional_prob(self):
         for i in range(self.map_size):
             for j in range(self.map_size):
                 char_i = self.get_char(i)
@@ -120,5 +96,3 @@ class Bigram(LanguageModel):
                 x = (self.bigram_map[i][j] + 0.5) / (self.count_char[i] + 26 * 0.5)
                 key = '(' + char_i + '|' + char_j + ')'
                 self.cond_probs[key] = x
-        LanguageModel.write_file_lang_model(self.cond_probs, 'bigram.txt')
-        return self.cond_probs
--- Run.py
+++ Run.py
@@ -1,55 +1,130 @@
 import Models
+import operator
+import math
+
+
+class WriterFile:
+
+    @staticmethod
+    def write_file_output(sentence_original, filename_number, EN_Un, FR_Un, GE_Un, EN_Bi, FR_Bi, GE_Bi, print_all):
+        with open('out' + str(filename_number) + '.txt', 'w') as f:
+            sentence = Models.LanguageModel.clean_sentence(sentence_original)
+            log_prob_en = 0
+            log_prob_ge = 0
+            log_prob_fr = 0
+            f.write(sentence_original + '\n\n')
+            print(sentence_original)
+            f.write('UNIGRAM MODEL:\n\n')
+            print('UNIGRAM MODEL:')
+            for c in sentence:
+                prob_en_uni = EN_Un.probs_alpha['(' + c + ')']
+                log_prob_en += math.log(prob_en_uni, 10)
+                prob_fr_uni = FR_Un.probs_alpha['(' + c + ')']
+                log_prob_fr += math.log(prob_fr_uni, 10)
+                prob_ge_uni = GE_Un.probs_alpha['(' + c + ')']
+                log_prob_ge += math.log(prob_ge_uni, 10)
+                f.write('UNIGRAM MODEL:' + c + '\n')
+                f.write('FRENCH: P(' + c + ') = ' + str(prob_fr_uni) + ' ==> log prob of sentence so far: ' + str(
+                    log_prob_fr) + '\n')
+                f.write(
+                    'ENGLISH: P(' + c + ') = ' + str(prob_en_uni) + ' ==> log prob of sentence so far: ' + str(
+                        log_prob_en) + '\n')
+                f.write('GERMAN: P(' + c + ') = ' + str(prob_ge_uni) + ' ==> log prob of sentence so far: ' + str(
+                    log_prob_ge) + '\n\n')
+                if print_all:
+                    print('UNIGRAM MODEL:' + c)
+                    print('FRENCH: P(' + c + ') = ' + str(prob_fr_uni) + ' ==> log prob of sentence so far: ' + str(
+                        log_prob_fr))
+                    print('ENGLISH: P(' + c + ') = ' + str(prob_en_uni) + ' ==> log prob of sentence so far: ' + str(
+                        log_prob_en))
+                    print('GERMAN: P(' + c + ') = ' + str(prob_ge_uni) + ' ==> log prob of sentence so far: ' + str(
+                        log_prob_ge))
+            if log_prob_en > log_prob_ge and log_prob_en > log_prob_fr:
+                f.write('According to the unigram model, the sentence is in English\n\n')
+                print('According to the unigram model, the sentence is in English')
+            elif log_prob_fr > log_prob_en and log_prob_fr > log_prob_ge is 'FR':
+                f.write('According to the unigram model, the sentence is in French\n\n')
+                print('According to the unigram model, the sentence is in French')
+            else:
+                f.write('According to the unigram model, the sentence is in German\n\n')
+                print('According to the unigram model, the sentence is in German')
+            f.write('----------------\n\n')
+            print('----------------\n')
+            log_prob_en = 0
+            log_prob_ge = 0
+            log_prob_fr = 0
+            f.write('BIGRAM MODEL:\n\n')
+            print('BIGRAM MODEL:')
+            for c in range(0, len(sentence) - 1):
+                index_i = sentence[c]
+                index_j = sentence[c + 1]
+                prob_en_bi = EN_Bi.cond_probs['(' + (index_i) + '|' + (index_j) + ')']
+                prob_fr_bi = FR_Bi.cond_probs['(' + (index_i) + '|' + (index_j) + ')']
+                prob_ge_bi = GE_Bi.cond_probs['(' + (index_i) + '|' + (index_j) + ')']
+                log_prob_en += math.log(prob_en_bi, 10)
+                log_prob_fr += math.log(prob_fr_bi, 10)
+                log_prob_ge += math.log(prob_ge_bi, 10)
+                f.write('BIGRAM MODEL: ' + sentence[c] + sentence[c + 1] + '\n')
+                f.write('FRENCH: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                    prob_fr_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_fr) + '\n')
+                f.write('ENGLISH: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                    prob_en_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_en) + '\n')
+                f.write('GERMAN: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                    prob_ge_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_ge) + '\n\n')
+                if print_all:
+                    print('BIGRAM MODEL: ' + sentence[c] + sentence[c + 1])
+                    print('FRENCH: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                        prob_fr_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_fr))
+                    print('ENGLISH: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                        prob_en_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_en))
+                    print('GERMAN: P(' + sentence[c + 1] + '|' + sentence[c] + ') = ' + str(
+                        prob_ge_bi) + ' ==> log prob of sentence so far: ' + str(log_prob_ge))
+
+            if log_prob_en > log_prob_ge and log_prob_en > log_prob_fr:
+                f.write('According to the bigram model, the sentence is in English\n')
+                print('According to the bigram model, the sentence is in English\n')
+            elif log_prob_fr > log_prob_en and log_prob_fr > log_prob_ge:
+                f.write('According to the bigram model, the sentence is in French\n')
+                print('According to the bigram model, the sentence is in French\n')
+            else:
+                f.write('According to the bigram model, the sentence is in German\n')
+                print('According to the bigram model, the sentence is in German\n')
+
+
 files_en = ['en-moby-dick.txt', 'en-the-little-prince.txt']
+model_EN_Un = Models.Unigram(files_en)
+model_EN_Un.write_file_lang_model(model_EN_Un.probs_alpha, 'unigramEN.txt')
+
 files_fr = ['fr-le-petit-prince.txt', 'fr-vingt-mille-lieues-sous-les-mers.txt']
-model_EN = Models.Unigram(files_en)
-# model_EN = Models.Unigram('en-the-little-prince.txt')
-# model_FR = Models.Unigram(files_fr)
-print(model_EN.probabilities_alpha())
-# sen2 = "What will the Japanese economy be like next year?"
-# sen3 = "She asked him if he was a student at this school."
-# sen4 = "I'm OK."
-# sen5 = "Birds build nests."
-# sen6 = "I hate AI."
-# sen7 = "L’oiseau vole."
-# sen8 = "Woody Allen parle."
-# sen9 = "Est-ce que l’arbitre est la?"
-# sen0 = "Cette phrase est en anglais."
-# sen = "J’aime l’IA."
-#
-# print(sen, "Prob in eng: ", model_EN.calc_prob_unigram(sen), "Prob in fre: ", model_FR.calc_prob_unigram(sen))
-# print(sen2, "Prob in eng: ", model_EN.calc_prob_unigram(sen2), "Prob in fre: ", model_FR.calc_prob_unigram(sen2))
-# print(sen3, "Prob in eng: ", model_EN.calc_prob_unigram(sen3), "Prob in fre: ", model_FR.calc_prob_unigram(sen3))
-# print(sen4, "Prob in eng: ", model_EN.calc_prob_unigram(sen4), "Prob in fre: ", model_FR.calc_prob_unigram(sen4))
-# print(sen5, "Prob in eng: ", model_EN.calc_prob_unigram(sen5), "Prob in fre: ", model_FR.calc_prob_unigram(sen5))
-# print(sen6, "Prob in eng: ", model_EN.calc_prob_unigram(sen6), "Prob in fre: ", model_FR.calc_prob_unigram(sen6))
-# print(sen7, "Prob in eng: ", model_EN.calc_prob_unigram(sen7), "Prob in fre: ", model_FR.calc_prob_unigram(sen7))
-# print(sen8, "Prob in eng: ", model_EN.calc_prob_unigram(sen8), "Prob in fre: ", model_FR.calc_prob_unigram(sen8))
-# print(sen9, "Prob in eng: ", model_EN.calc_prob_unigram(sen9), "Prob in fre: ", model_FR.calc_prob_unigram(sen9))
-# print(sen0, "Prob in eng: ", model_EN.calc_prob_unigram(sen0), "Prob in fre: ", model_FR.calc_prob_unigram(sen0))
+model_FR_Un = Models.Unigram(files_fr)
+model_FR_Un.write_file_lang_model(model_FR_Un.probs_alpha, 'unigramFR.txt')
 
-# model_EN = Models.Bigram('en-moby-dick.txt', 'en-the-little-prince.txt')
-# model_FR = Models.Bigram('fr-le-petit-prince.txt', 'fr-vingt-mille-lieues-sous-les-mers.txt')
-# print(model_EN.conditional_prob())
-#
-# sen2 = "What will the Japanese economy be like next year?"
-# sen3 = "She asked him if he was a student at this school."
-# sen4 = "I'm OK."
-# sen5 = "Birds build nests."
-# sen6 = "I hate AI."
-# sen7 = "L’oiseau vole."
-# sen8 = "Woody Allen parle."
-# sen9 = "Est-ce que l’arbitre est la?"
-# sen0 = "Cette phrase est en anglais."
-# sen = "J’aime l’IA."
-#
-# print(sen, "Prob in eng: ", model_EN.calc_prob_bigram(sen), "Prob in fre: ", model_FR.calc_prob_bigram(sen))
-# print(sen2, "Prob in eng: ", model_EN.calc_prob_bigram(sen2), "Prob in fre: ", model_FR.calc_prob_bigram(sen2))
-# print(sen3, "Prob in eng: ", model_EN.calc_prob_bigram(sen3), "Prob in fre: ", model_FR.calc_prob_bigram(sen3))
-# print(sen4, "Prob in eng: ", model_EN.calc_prob_bigram(sen4), "Prob in fre: ", model_FR.calc_prob_bigram(sen4))
-# print(sen5, "Prob in eng: ", model_EN.calc_prob_bigram(sen5), "Prob in fre: ", model_FR.calc_prob_bigram(sen5))
-# print(sen6, "Prob in eng: ", model_EN.calc_prob_bigram(sen6), "Prob in fre: ", model_FR.calc_prob_bigram(sen6))
-# print(sen7, "Prob in eng: ", model_EN.calc_prob_bigram(sen7), "Prob in fre: ", model_FR.calc_prob_bigram(sen7))
-# print(sen8, "Prob in eng: ", model_EN.calc_prob_bigram(sen8), "Prob in fre: ", model_FR.calc_prob_bigram(sen8))
-# print(sen9, "Prob in eng: ", model_EN.calc_prob_bigram(sen9), "Prob in fre: ", model_FR.calc_prob_bigram(sen9))
-# print(sen0, "Prob in eng: ", model_EN.calc_prob_bigram(sen0), "Prob in fre: ", model_FR.calc_prob_bigram(sen0))
+files_ge = ['Kritik-der-reinen-Vernunft.txt', 'Buddenbrooks.txt']
+model_GE_Un = Models.Unigram(files_ge)
+model_GE_Un.write_file_lang_model(model_GE_Un.probs_alpha, 'unigramOT.txt')
 
+sentences = ["What will the Japanese economy be like next year?", "She asked him if he was a student at this school.",
+             "I'm OK.", "Birds build nests.", "I hate AI.", "L’oiseau vole.", "Woody Allen parle.",
+             "Est-ce que l’arbitre est la?",
+             "Cette phrase est en anglais.", "J’aime l’IA.", "Guten Abend!", "I l"]
+
+# sentences = ["What will the Japanese economy be like next year?"]
+model_EN_Bi = Models.Bigram(files_en)
+model_EN_Bi.write_file_lang_model(model_EN_Bi.cond_probs, 'bigramEn.txt')
+model_FR_Bi = Models.Bigram(files_fr)
+model_FR_Bi.write_file_lang_model(model_FR_Bi.cond_probs, 'bigramFR.txt')
+model_GE_Bi = Models.Bigram(files_ge)
+model_GE_Bi.write_file_lang_model(model_GE_Bi.cond_probs, 'bigramGE.txt')
+
+for i in range(0, len(sentences)):
+    # prob_en = model_EN_Un.calc_prob_unigram(sentences[i])
+    # prob_fr = model_FR_Un.calc_prob_unigram(sentences[i])
+    # prob_ge = model_GE_Un.calc_prob_unigram(sentences[i])
+    # prob_map = {'EN': prob_en, 'FR': prob_fr, 'GE': prob_ge}
+    # sorted_list = sorted(prob_map.items(), key=operator.itemgetter(1), reverse=True)
+    # sorted_map = dict(sorted_list)
+    WriterFile.write_file_output(sentences[i], (i + 1), model_EN_Un, model_FR_Un, model_GE_Un,
+                                 model_EN_Bi, model_FR_Bi, model_GE_Bi, False)
+    # print(sentence, sorted_map[list(sorted_map.keys())[0]])
+    # print(sentences[i], next(iter(sorted_map)))
+    # print(sorted_map)
